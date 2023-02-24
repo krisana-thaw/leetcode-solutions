37bcc58eacb49072be1422f43d6ec8239d3142ce
Krisana Thawornwatthanaphol <64942894+krisana-thaw@users.noreply.github.com>
2023-02-24 20:56:27 +0700

    Time: 58 ms (95.21%), Space: 42.4 MB (97.95%) - LeetHub

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.ts
@@ -0,0 +1,17 @@
+function countDigits(num: number): number {
+    let count = 0 ;
+    const n = num;
+    
+    while(num > 1){
+        if(n % (num % 10) === 0){
+            count++;
+        }
+        num = Math.floor(num / 10);
+    }
+    
+    if(num === 1){
+        count++;
+    }
+    
+    return count
+};
