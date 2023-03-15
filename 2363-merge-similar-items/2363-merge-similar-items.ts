66a98135278d204bd061687ffed5d181109cfa75
Krisana Thawornwatthanaphol <64942894+krisana-thaw@users.noreply.github.com>
2023-03-15 16:50:16 +0700

    Time: 115 ms (19.05%), Space: 50.7 MB (26.19%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.ts
+++ 2363-merge-similar-items/2363-merge-similar-items.ts
@@ -3,11 +3,11 @@ function mergeSimilarItems(items1: number[][], items2: number[][]): number[][] {
     
     for (let [value, weight] of items1.concat(items2)) {
         if (!map.has(value)) {
-            map.set(value, weight)
+            map.set(value, weight);
         } else {
-            map.set(value, map.get(value) + weight)
+            map.set(value, map.get(value) + weight);
         }
     }
 
-    return Array.from(map.entries()).sort((a,b) => a[0] - b[0])
+    return [...map.entries()].sort((a,b) => a[0] - b[0]);
 };
