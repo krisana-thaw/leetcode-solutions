f095cf3663b97f079b0ae3c8ae9f6796e6903ef5
Krisana Thawornwatthanaphol <64942894+krisana-thaw@users.noreply.github.com>
2023-03-15 17:07:31 +0700

    Time: 58 ms (88.46%), Space: 42.3 MB (98.72%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.ts
@@ -0,0 +1,11 @@
+function repeatedCharacter(s: string): string {
+    const set = new Set();
+    
+    for (let i = 0; i < s.length; i++) {
+        const curr = s[i]
+        if(set.has(curr)) {
+             return curr 
+        }
+        set.add(curr);
+    }
+};
