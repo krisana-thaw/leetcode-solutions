dad6298e31650a6da653ba965d34e5962ad3f074
Krisana Thawornwatthanaphol <64942894+krisana-thaw@users.noreply.github.com>
2023-03-15 17:09:28 +0700

    Time: 56 ms (92.31%), Space: 43.1 MB (65.38%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.ts
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.ts
@@ -2,9 +2,9 @@ function repeatedCharacter(s: string): string {
     const set = new Set();
     
     for (let i = 0; i < s.length; i++) {
-        const curr = s[i]
+        const curr = s[i];
         if(set.has(curr)) {
-             return curr 
+             return curr; 
         }
         set.add(curr);
     }
