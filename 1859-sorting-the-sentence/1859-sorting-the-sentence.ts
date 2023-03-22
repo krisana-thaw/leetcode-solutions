62f4d7dad03f76d3abcffd2ccee96a9f6fc1da4d
Krisana Thawornwatthanaphol <64942894+krisana-thaw@users.noreply.github.com>
2023-03-22 14:34:08 +0700

    Time: 64 ms (75.49%), Space: 43.2 MB (41.18%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.ts
@@ -0,0 +1,21 @@
+function sortSentence(s: string): string {
+    const sentenceList = s.split(' ');
+    
+    const map = new Map();
+    for(let i = 0; i<sentenceList.length; i++){
+        const index = sentenceList[i].substr(sentenceList[i].length - 1);
+        const sentence = sentenceList[i].substr(0, sentenceList[i].length - 1);
+        map.set((Number(index)-1), sentence);
+    }
+    
+    const result = [...map].sort((a,b) => a[0] - b[0]).map((exemple, index, array) => exemple[1]).join(' ');
+    return result;
+};
+
+
+
+
+
+
+
+
